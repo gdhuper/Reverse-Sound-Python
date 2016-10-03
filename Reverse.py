f8ae1ea477af07c6afcb435a3801b80c08191421
Gurpreet Singh <gdhuper@gmail.com>
2016-10-03 12:36:36 -0700

    implemented to run from command line

--- Reverse.py
+++ Reverse.py
@@ -1,36 +1,55 @@
 #!/usr/bin/python
 from Stack import Stack
+from decimal import Decimal
 import sys, getopt
 import os
 import fileinput
+import argparse
 
-fileName = None
-fileName2 = None
+ifile = None
+ofile = None
+
 
 class Reverse():
-	"""class Reverse to reverse the sound file"""
-	def __init__(self, filename):
-		self.fileName = fileName
+    """class Reverse to reverse the sound file"""
+    def __init__(self, ifilename, ofilename):
+        self.ifile = ifilename
+        self.ofile = ofilename
 
-	def open_read_file(argv):
-		inputfile = ''
-		outputfile = ''
-		try:
-			opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
-		except getopt.GetoptError:
-			print ('Reverse.py -i <inputfile> -o <outputfile>')
-			sys.exit(2)
-		for opt, arg in opts:
-			if opt == '-h':
-				print ('Reverse.py -i <inputfile> -o <outputfile>')
-			elif opt in ("-i", "--ifile"):
-				inputfile = arg
-			elif opt in ("-o", "--ofile"):
-				outputfile = arg
+    def open_read_file(self, ifile, ofile):
+        s = Stack()
+        fileIn = open(self.ifile, "r")
+        #fileOut = open(self.ofile, "w")
+
+        str = fileIn.readline()
+        list = []
+        list = str.split(" ")
+        sampleRate = list[3]
+        counter = 0
+        while fileIn.readline() != None:
+            strtemp = fileIn.readline()
+            if 'str' in strtemp:
+                break
+            else:
+                print(strtemp)
+                counter += 1
+        print(counter)
 
 
-r =  Reverse(filename)
-Reverse.open_read_file(sys.arg[1:])
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Read and write file')
+    parser.add_argument("--ifile", help="Input file path/name")
+    parser.add_argument("--ofile", help="Output file path/name")
+
+    args = parser.parse_args()
+
+
+    r = Reverse(args.ifile, args.ofile)
+    r.open_read_file(ifile, ofile)
+
+
+
+
 
 
 
